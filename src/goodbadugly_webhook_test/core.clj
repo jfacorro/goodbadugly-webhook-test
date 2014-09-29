0f76ee287ee18d3c3de9bea87f3f1193612df3f5
Juan Facorro <juan.facorro@gmail.com>
2014-09-29 17:20:25 -0300

    Introduce bad, ugly code.

--- src/goodbadugly_webhook_test/core.clj
+++ src/goodbadugly_webhook_test/core.clj
@@ -1,6 +1,17 @@
 (ns goodbadugly-webhook-test.core)
 
+(= 1)
+
 (defn foo
+  "I don't do a whole lot."
+  [x]
+  :this-is-a-warning
+  (println x "Hello, World!"))
+
+(-> 1
+ (= 2))
+
+(defn bar
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
