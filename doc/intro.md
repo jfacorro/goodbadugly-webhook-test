6bfef30ad5d86b5bd74782c08b292737d33e4a95
Juan Facorro <juan.facorro@gmail.com>
2014-09-29 17:13:55 -0300

    Created project.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to goodbadugly-webhook-test
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/goodbadugly_webhook_test/core_test.clj
@@ -0,0 +1,7 @@
+(ns goodbadugly-webhook-test.core-test
+  (:require [clojure.test :refer :all]
+            [goodbadugly-webhook-test.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
